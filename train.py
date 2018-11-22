e5b84430c24a207d487413603f23034730545bde
Brennan Gebotys <gebob19.git@gmail.com>
2018-11-21 23:36:46 -0500

    updated train with callbacks and logger

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.losses import mean_squared_error
 from keras.optimizers import RMSprop
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
 
 from model import mobnet
 
@@ -70,27 +71,56 @@ if __name__ == '__main__':
 
     parser.add_option('-b', '--batch_size',
     action="store", dest="bs",
-    help="b", default="16")
+    help="b", default="32")
 
     options, args = parser.parse_args()
 
     epochs = int(options.epochs)
     BATCH_SIZE = int(options.bs)
 
+    # learning_rate = [1e-2, 1e-3, 3e-3]
+
+
     # testdf = pd.read_csv('test-df.csv')
-    traindf = pd.read_csv('train-df.csv')
+    # reduce dset 25% of original size to evaulate models faster
+    # 2.4 million * 0.15 = 360,000 frame dset
+    traindf = pd.read_csv('train-df.csv').sample(frac=0.15) 
     train, val = train_test_split(traindf, test_size=0.1)
 
+    model_name = "basemodel_imgnet_224x224"
     model = mobnet((HEIGHT, WIDTH, 3))
     model.compile(loss = {'y_xcam': mean_squared_error,
                         'y_ycam': mean_squared_error},
-                optimizer = RMSprop())
-    #               metrics = ['accuracy', 'mae'])
+                  optimizer = RMSprop(),
+                  metrics = ['accuracy', 'mae'])
+
+    callbacks = [
+        ReduceLROnPlateau(monitor='val_loss',
+                        factor=0.2,
+                        patience=4,
+                        verbose=1,
+                        min_delta=1e-5),
+        # ModelCheckpoint(monitor='val_loss',
+        #                 filepath='best_weights.hdf5',
+        #                 save_best_only=True,
+        #                 verbose=1)
+    ]
 
     model.fit_generator(generator=train_generator(train),
                             steps_per_epoch=np.ceil(float(len(train)) / float(BATCH_SIZE)),
                             epochs=epochs,
                             verbose=1,
-    #                         callbacks=callbacks,
+                            callbacks=callbacks,
                             validation_data=train_generator(val),
                             validation_steps=np.ceil(float(len(val)) / float(BATCH_SIZE)))
+
+    metrics = model.evaluate_generator(train_generator(val), steps=np.ceil(float(len(val)) / float(BATCH_SIZE))
+    metric_names = model.metrics_names
+
+    with open('results.txt', 'a') as f:
+        f.write("model {} results: ".format(model_name))
+        f.write(str(metric_names))
+        f.write(str(metrics))
+        f.write("\n")
+
+
